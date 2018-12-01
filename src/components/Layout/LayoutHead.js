228996db4a2acc81a40f43c4419cb807b0656703
Kristie Yorkston <kyorkston@gmail.com>
2018-12-01 20:48:18 +0000

    edit readme and move files into folders

--- src/components/Layout/LayoutHead.js
+++ src/components/Layout/LayoutHead.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
 
 const LayoutHead = ({ data, children }) => (
   <div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 
-import Layout from '../components/layout'
+import Layout from '../components/Layout/layout.js'
 
-const IndexPage = ({ data }) => (
+const IndexPage = ({data}) => (
   <Layout images={data}>
     <i>Under construction</i>
   </Layout>
@@ -12,36 +11,37 @@ const IndexPage = ({ data }) => (
 export default IndexPage
 
 export const fluidImage = graphql`
-  fragment fluidImage on File {
-    childImageSharp {
-      fluid(maxWidth: 800) {
-        ...GatsbyImageSharpFluid
-      }
+fragment fluidImage on File {
+  childImageSharp {
+    fluid(maxWidth: 800) {
+      ...GatsbyImageSharpFluid
     }
   }
+}
 `
+
 export const imageQuery = graphql`
-  query {
-    roadTrip: file(relativePath: { regex: "/roadTrip.jpg/" }) {
-      ...fluidImage
-    }
-    amsterdam: file(relativePath: { regex: "/amsterdam.jpg/" }) {
-      ...fluidImage
-    }
-    bathStreet: file(relativePath: { regex: "/bathStreet.jpg/" }) {
-      ...fluidImage
-    }
-    edinburghUni: file(relativePath: { regex: "/edinburghUni.jpg/" }) {
-      ...fluidImage
-    }
-    parkheadChurch: file(relativePath: { regex: "/parkheadChurch.jpg/" }) {
-      ...fluidImage
-    }
-    queensParkChurch: file(relativePath: { regex: "/queensParkChurch.jpg/" }) {
-      ...fluidImage
-    }
-    shawlandsImage: file(relativePath: { regex: "/shawlandsImage.jpg/" }) {
-      ...fluidImage
-    }
+query {
+  roadTrip: file(relativePath: { regex: "/roadTrip.jpg/" }) {
+    ...fluidImage
   }
+  amsterdam: file(relativePath: { regex: "/amsterdam.jpg/" }) {
+    ...fluidImage
+  }
+  bathStreet: file(relativePath: { regex: "/bathStreet.jpg/" }) {
+    ...fluidImage
+  }
+  edinburghUni: file(relativePath: { regex: "/edinburghUni.jpg/" }) {
+    ...fluidImage
+  }
+  parkheadChurch: file(relativePath: { regex: "/parkheadChurch.jpg/" }) {
+    ...fluidImage
+  }
+  queensParkChurch: file(relativePath: { regex: "/queensParkChurch.jpg/" }) {
+    ...fluidImage
+  }
+  shawlandsImage: file(relativePath: { regex: "/shawlandsImage.jpg/" }) {
+    ...fluidImage
+  }
+}
 `
