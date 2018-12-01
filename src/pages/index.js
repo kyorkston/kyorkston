5bb9f317c6541894d21402500946ff4b17b9a0da
Kristie Yorkston <kyorkston@gmail.com>
2018-12-01 20:50:19 +0000

    fix indent of graphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,37 +11,37 @@ const IndexPage = ({data}) => (
 export default IndexPage
 
 export const fluidImage = graphql`
-fragment fluidImage on File {
-  childImageSharp {
-    fluid(maxWidth: 800) {
-      ...GatsbyImageSharpFluid
+  fragment fluidImage on File {
+    childImageSharp {
+      fluid(maxWidth: 800) {
+        ...GatsbyImageSharpFluid
+      }
     }
   }
-}
 `
 
 export const imageQuery = graphql`
-query {
-  roadTrip: file(relativePath: { regex: "/roadTrip.jpg/" }) {
-    ...fluidImage
+  query {
+    roadTrip: file(relativePath: { regex: "/roadTrip.jpg/" }) {
+      ...fluidImage
+    }
+    amsterdam: file(relativePath: { regex: "/amsterdam.jpg/" }) {
+      ...fluidImage
+    }
+    bathStreet: file(relativePath: { regex: "/bathStreet.jpg/" }) {
+      ...fluidImage
+    }
+    edinburghUni: file(relativePath: { regex: "/edinburghUni.jpg/" }) {
+      ...fluidImage
+    }
+    parkheadChurch: file(relativePath: { regex: "/parkheadChurch.jpg/" }) {
+      ...fluidImage
+    }
+    queensParkChurch: file(relativePath: { regex: "/queensParkChurch.jpg/" }) {
+      ...fluidImage
+    }
+    shawlandsImage: file(relativePath: { regex: "/shawlandsImage.jpg/" }) {
+      ...fluidImage
+    }
   }
-  amsterdam: file(relativePath: { regex: "/amsterdam.jpg/" }) {
-    ...fluidImage
-  }
-  bathStreet: file(relativePath: { regex: "/bathStreet.jpg/" }) {
-    ...fluidImage
-  }
-  edinburghUni: file(relativePath: { regex: "/edinburghUni.jpg/" }) {
-    ...fluidImage
-  }
-  parkheadChurch: file(relativePath: { regex: "/parkheadChurch.jpg/" }) {
-    ...fluidImage
-  }
-  queensParkChurch: file(relativePath: { regex: "/queensParkChurch.jpg/" }) {
-    ...fluidImage
-  }
-  shawlandsImage: file(relativePath: { regex: "/shawlandsImage.jpg/" }) {
-    ...fluidImage
-  }
-}
 `
