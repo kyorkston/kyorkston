dc51dcaabf42c824a672b9290307e86233e984cd
Kristie <kyorkston@gmail.com>
2022-04-07 20:44:14 +0100

    update dependencies, remove images

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { graphql } from 'gatsby'
 
 import Layout from '../../components/Layout/layout'
 
 const BlogHome = (props) => {
   return (
-    <Layout images={props.data}>
+    <Layout>
       <ul>
         <li><a href='/25-12-2018'>Making an HTML5 Canvas app using express</a></li>
       </ul>
@@ -14,29 +13,3 @@ const BlogHome = (props) => {
 }
 
 export default BlogHome
-
-export const imageQuery = graphql`
-  query {
-    roadTrip: file(relativePath: { regex: "/roadTrip.jpg/" }) {
-      ...fluidImage
-    }
-    amsterdam: file(relativePath: { regex: "/amsterdam.jpg/" }) {
-      ...fluidImage
-    }
-    bathStreet: file(relativePath: { regex: "/bathStreet.jpg/" }) {
-      ...fluidImage
-    }
-    edinburghUni: file(relativePath: { regex: "/edinburghUni.jpg/" }) {
-      ...fluidImage
-    }
-    parkheadChurch: file(relativePath: { regex: "/parkheadChurch.jpg/" }) {
-      ...fluidImage
-    }
-    queensParkChurch: file(relativePath: { regex: "/queensParkChurch.jpg/" }) {
-      ...fluidImage
-    }
-    shawlandsImage: file(relativePath: { regex: "/shawlandsImage.jpg/" }) {
-      ...fluidImage
-    }
-  }
-`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import ReactLoading from 'react-loading'
-import { graphql } from 'gatsby'
 
 import Layout from '../components/Layout/layout'
 import Bio from '../components/Bio'
@@ -15,13 +13,7 @@ const IndexPage = (props) => {
   return (
     <div>
       {loading ? (
-        <ReactLoading
-          style={{ display: 'flex', justifyContent: 'center' }}
-          type={'spin'}
-          color={'grey'}
-          height={80}
-          width={80}
-        />
+        <>Loading</>
       ) : (
           <Layout images={props.data}>
             <Bio />
@@ -32,39 +24,3 @@ const IndexPage = (props) => {
 }
 
 export default IndexPage
-
-export const fluidImage = graphql`
-  fragment fluidImage on File {
-    childImageSharp {
-      fluid(maxWidth: 700) {
-        ...GatsbyImageSharpFluid
-      }
-    }
-  }
-`
-
-export const imageQuery = graphql`
-  query {
-    roadTrip: file(relativePath: { regex: "/roadTrip.jpg/" }) {
-      ...fluidImage
-    }
-    amsterdam: file(relativePath: { regex: "/amsterdam.jpg/" }) {
-      ...fluidImage
-    }
-    bathStreet: file(relativePath: { regex: "/bathStreet.jpg/" }) {
-      ...fluidImage
-    }
-    edinburghUni: file(relativePath: { regex: "/edinburghUni.jpg/" }) {
-      ...fluidImage
-    }
-    parkheadChurch: file(relativePath: { regex: "/parkheadChurch.jpg/" }) {
-      ...fluidImage
-    }
-    queensParkChurch: file(relativePath: { regex: "/queensParkChurch.jpg/" }) {
-      ...fluidImage
-    }
-    shawlandsImage: file(relativePath: { regex: "/shawlandsImage.jpg/" }) {
-      ...fluidImage
-    }
-  }
-`
